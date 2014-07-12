27dc17775614ad50d1043269f0a3e68198818640
Cécile Tonglet <cecile.tonglet@gmail.com>
2014-07-12 18:19:13 +0200

    FORK -- let's do it in an other way

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 
 version = "0.9.4"
 
-setup(name="fastimport",
+setup(name="yafastimport",
       description="VCS fastimport/fastexport parser",
       version=version,
-      author="Canonical Ltd",
-      author_email="bazaar@lists.canonical.com",
+      author="Cecile Tonglet",
+      author_email="cecile.tonglet@gmail.com",
       license="GNU GPL v2 or later",
-      url="https://launchpad.net/python-fastimport",
+      url="https://github.com/cecton/python-yafastimport",
       packages=['fastimport', 'fastimport.tests', 'fastimport.processors'])
